ef4280aba1c996dd2bba8ead9c0a649890a5163d
pfortune <pfortune@gmail.com>
2020-02-06 09:19:35 +0000

    first init

--- promises.js
+++ promises.js
@@ -1,153 +1,152 @@
-const API_KEY = "4TQU66HTCC74MR96BRQY44E6ZB6RIKJU";
-const FORMAT = "output_format=JSON";
+const API_KEY = '';
+const FORMAT = 'output_format=JSON';
 const URL = `https://www.boardgamer.ie/api`;
 
 //
 async function getOrders() {
-  const orders = await getOrderIds();
-  const promises = [];
+	const orderIds = await getOrderIds();
+	const orders = [];
 
-  orders.forEach(order => {
-    promises.push(getOrderDetails(order.id));
-  });
+	orderIds.forEach((order) => {
+		orders.push(getOrderDetails(order.id));
+	});
 
-  return Promise.all(promises).then(order => console.log(order));
+	return Promise.all(orders).then((order) => console.log(order));
 }
 
 //Returns the IDs of every order with a particular status
 function getOrderIds() {
-  return fetch(
-    `${URL}/orders/?ws_key=${API_KEY}&filter[current_state]=[2]&${FORMAT}&rand=${Math.floor(
-      Math.random() * 1000000
-    )}`
-  )
-    .then(res => {
-      return res.json();
-    })
-    .then(res => res.orders);
+	return fetch(
+		`${URL}/orders/?ws_key=${API_KEY}&filter[current_state]=[2]&${FORMAT}&rand=${Math.floor(
+			Math.random() * 1000000
+		)}`
+	)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => res.orders);
 }
 
 // fetches products and various ids needed for future calls
 function getOrderDetails(id) {
-  return fetch(`${URL}/orders/${id}/?ws_key=${API_KEY}&${FORMAT}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      return {
-        id: res.order.id,
-        id_address_delivery: res.order.id_address_delivery,
-        id_carrier: res.order.id_carrier,
-        id_customer: res.order.id_customer,
-        products: res.order.associations.order_rows
-      };
-    })
-    .then(order => combineOrderDetails(order));
+	return fetch(`${URL}/orders/${id}/?ws_key=${API_KEY}&${FORMAT}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			return {
+				id: res.order.id,
+				id_address_delivery: res.order.id_address_delivery,
+				id_carrier: res.order.id_carrier,
+				id_customer: res.order.id_customer,
+				products: res.order.associations.order_rows
+			};
+		})
+		.then((order) => combineOrderDetails(order));
 }
 
 // fetches address details, uses id_address_delivery from getOrderDetails
 function getAddressDetails(id) {
-  return fetch(`${URL}/addresses/${id}/?ws_key=${API_KEY}&${FORMAT}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      return res.address;
-    })
-    .then(res => {
-      return {
-        id_state: res.id_state,
-        company: res.company,
-        firstname: res.firstname,
-        lastname: res.lastname,
-        address1: res.address1,
-        address2: res.address2,
-        postcode: res.postcode,
-        city: res.city,
-        other: res.other,
-        phone: formatPhoneNumber(res.phone.replace),
-        phone_mobile: formatPhoneNumber(res.phone_mobile)
-      };
-    });
+	return fetch(`${URL}/addresses/${id}/?ws_key=${API_KEY}&${FORMAT}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			return res.address;
+		})
+		.then((res) => {
+			return {
+				id_state: res.id_state,
+				company: res.company,
+				firstname: res.firstname,
+				lastname: res.lastname,
+				address1: res.address1,
+				address2: res.address2,
+				postcode: res.postcode,
+				city: res.city,
+				other: res.other,
+				phone: formatPhoneNumber(res.phone.replace),
+				phone_mobile: formatPhoneNumber(res.phone_mobile)
+			};
+		});
 }
 
 // fetches carrier information, uses id_carrier from getOrderDetails
 function getCarrierDetails(id) {
-  return fetch(`${URL}/carriers/${id}/?ws_key=${API_KEY}&${FORMAT}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      return { id_carrier: res.carrier.id, carrier: res.carrier.name };
-    });
+	return fetch(`${URL}/carriers/${id}/?ws_key=${API_KEY}&${FORMAT}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			return { id_carrier: res.carrier.id, carrier: res.carrier.name };
+		});
 }
 
 // fetches the customer email address, uses id_customer from getOrderDetails
 function getCustomerDetails(id) {
-  return fetch(`${URL}/customers/${id}/?ws_key=${API_KEY}&${FORMAT}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      return {
-        email: res.customer.email
-      };
-    });
+	return fetch(`${URL}/customers/${id}/?ws_key=${API_KEY}&${FORMAT}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			return {
+				email: res.customer.email
+			};
+		});
 }
 
 // fetches the name of state/county, uses id_state from getAddressDetails
 function getStateDetails(id) {
-  return fetch(`${URL}/states/${id}/?ws_key=${API_KEY}&${FORMAT}`)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      return {
-        state: res.state.name
-      };
-    });
+	return fetch(`${URL}/states/${id}/?ws_key=${API_KEY}&${FORMAT}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			return {
+				state: res.state.name
+			};
+		});
 }
 
 // combines the customer, address, carrier, state, and orderdetails together
 function combineOrderDetails(orderDetails) {
-  return Promise.all([
-    getCustomerDetails(orderDetails.id_customer),
-    getAddressDetails(orderDetails.id_address_delivery),
-    getCarrierDetails(orderDetails.id_carrier),
-    orderDetails
-  ])
-    .then(([customer, address, carrier]) => {
-      return { ...customer, ...address, ...carrier, ...orderDetails };
-    })
-    .then(order => {
-      return Promise.all([getStateDetails(order.id_state), order]);
-    })
-    .then(([state, order]) => {
-      return { ...state, ...order };
-    })
-    .then(orders => {
-      return assignCollectionOrders(orders);
-    });
+	return Promise.all([
+		getCustomerDetails(orderDetails.id_customer),
+		getAddressDetails(orderDetails.id_address_delivery),
+		getCarrierDetails(orderDetails.id_carrier),
+		orderDetails
+	])
+		.then(([ customer, address, carrier ]) => {
+			return { ...customer, ...address, ...carrier, ...orderDetails };
+		})
+		.then((order) => {
+			return Promise.all([ getStateDetails(order.id_state), order ]);
+		})
+		.then(([ state, order ]) => {
+			return { ...state, ...order };
+		})
+		.then((orders) => {
+			return assignCollectionOrders(orders);
+		});
 }
 
 // if an order has the carrier with id 193 it is marked for collection
 function assignCollectionOrders(order) {
-  const COLLECTION = "193";
-  if (order.id_carrier === COLLECTION) {
-    order.address1 = order.address2 = order.city = order.company = order.state =
-      "Collection";
-  }
+	const COLLECTION = '193';
+	if (order.id_carrier === COLLECTION) {
+		order.address1 = order.address2 = order.city = order.company = order.state = 'Collection';
+	}
 
-  return order;
+	return order;
 }
 
 function formatPhoneNumber(number) {
-  return number.replace(/\D+/g, "");
+	return number.replace(/\D+/g, '');
 }
 
 function buildCSV() {
-  let csv =
-    "Company,Firstname,Lastname,Order,Date,Quantity,Customer,Address1,Address2,Address3,City,State,Postcode,Other,Country,Email,Sku,Phone\n";
+	let csv =
+		'Company,Firstname,Lastname,Order,Date,Quantity,Customer,Address1,Address2,Address3,City,State,Postcode,Other,Country,Email,Sku,Phone\n';
 }
 
-document.querySelector(".status").addEventListener("click", getOrders);
+document.querySelector('.status').addEventListener('click', getOrders);
